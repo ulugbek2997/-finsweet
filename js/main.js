ee22347a2fa5b40abd8f6593c43e2bb8bbd6bf8a
ulugbek2997 <rustamovulugbek831@gmail.com>
2022-02-02 19:22:41 +0500

    {finsweet

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  let header__right = document.querySelector('.header__right');
+  header__right.classList.toggle('header__right-togle'); 
+}
---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".aboutUs__supporters-swiper", {
+  slidesPerView: 6,
+  spaceBetween: 96,
+  speed: 500,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+});
